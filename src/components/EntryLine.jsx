69957605d87a7e2f972488fe76d03510526fc6d3
yichien <kevinyichienlee@gmail.com>
2024-08-23 23:03:55 +0800

    creating a model to edit the entries

--- src/components/EntryLine.jsx
+++ src/components/EntryLine.jsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, Icon, Segment } from "semantic-ui-react";
+import ModalEdit from "./ModalEdit";
 
 const EntryLine = ({ id, description, value, isExpense, deleteEntry }) => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <Segment color={isExpense ? "red" : "green"}>
-      <Grid columns={3} textAlign="right">
-        <Grid.Row>
-          <Grid.Column width={10} textAlign="left">
-            {description}
-          </Grid.Column>
-          <Grid.Column width={3} textAlign="right">
-            {value}
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <Icon name="edit" bordered />
-            <Icon name="trash" bordered onClick={() => deleteEntry(id)} />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
+    <>
+      <Segment color={isExpense ? "red" : "green"}>
+        <Grid columns={3} textAlign="right">
+          <Grid.Row>
+            <Grid.Column width={10} textAlign="left">
+              {description}
+            </Grid.Column>
+            <Grid.Column width={3} textAlign="right">
+              {value}
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name="edit" bordered onClick={() => setIsOpen(true)} />
+              <Icon name="trash" bordered onClick={() => deleteEntry(id)} />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <ModalEdit isOpen={isOpen} />
+    </>
   );
 };
 
---
+++ src/components/ModalEdit.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Modal } from "semantic-ui-react";
+
+const ModalEdit = ({ isOpen }) => {
+  return <Modal open={isOpen}></Modal>;
+};
+
+export default ModalEdit;
